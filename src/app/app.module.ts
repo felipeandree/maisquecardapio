8c70fb2912a5f890ad9fdc933fb7cd55e064b9ee
felipeandree <felipeandre8@outlook.com>
2024-01-11 20:09:28 -0300

    update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerCardapioComponent } from './banner-cardapio/banner-cardapio.component';
-import { LogoComponent } from './logo/logo.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerCardapioComponent,
-    LogoComponent
   ],
   imports: [
     BrowserModule,
